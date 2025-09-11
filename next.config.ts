8a9d769abb72db0a1747709760407c9afe11e6ce
AO PLUS <aoplusin@gmail.com>
2025-09-11 12:55:36 +0000

    while the site is published to github getting the following error [ailed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type {NextConfig} from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repo = 'prodory';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isGithubActions ? '/prodory' : '',
-  assetPrefix: isGithubActions ? '/prodory/' : '',
+  basePath: isGithubActions ? `/${repo}` : '',
+  assetPrefix: isGithubActions ? `/${repo}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
